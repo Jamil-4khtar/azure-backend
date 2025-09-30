6f2eb63f187394854cba156b5d3a6e60469a73d8
Jamil-4khtar <jamil4khtar@gmail.com>
2025-09-30 16:14:07 +0530

    feat: Add validation middleware and schemas for user registration and login

--- src/features/auth/auth.route.js
+++ src/features/auth/auth.route.js
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import { register, login, getProfile } from './auth.controller.js';
 import { isAuthenticated } from '../../middleware/isAuthenticated.js';
 import { loginHourlyLimiter, loginLimiter, registerLimiter } from '../../middleware/rateLimiter.js';
+import { loginSchema, registerSchema, validate } from '../../middleware/validation.js';
 
 const router = Router();
 
-router.post('/register', registerLimiter, register);
-router.post('/login', loginLimiter, loginHourlyLimiter, login);
+router.post('/register', registerLimiter, validate(registerSchema), register);
+router.post('/login', loginLimiter, loginHourlyLimiter, validate(loginSchema), login);
 router.get("/me", isAuthenticated, getProfile)
 
 export default router;	
